20f1632722abb6290264a084be042be2a527236f
leonardus <leonardus@leonardus.me>
2019-01-25 17:57:34 -0500

    Add additional datarefs for future use

--- Scripts/XP-RPC.lua
+++ Scripts/XP-RPC.lua
@@ -6,6 +6,10 @@ local XPlane11SteamId = "269950"
 
 function update_rpc()
     dataref("aircraft", "sim/aircraft/view/acf_descrip", "readable")
+    dataref("tailnumber", "sim/aircraft/view/acf_tailnum", "readable")
+    dataref("icao", "sim/aircraft/view/acf_ICAO", "readable")
+    dataref("heading_magnetic", "sim/flightmodel/position/mag_psi", "readable")
+    dataref("heading_true", "sim/flightmodel/position/true_psi", "readable")
     dataref("kias", "sim/flightmodel/position/indicated_airspeed", "readable")
     dataref("alt", "sim/flightmodel/position/elevation", "readable")
     alt = alt * 3.28084 -- convert m to ft
